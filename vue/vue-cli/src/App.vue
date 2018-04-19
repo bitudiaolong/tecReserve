4f617f747aecaae8cd84b718bb8d3ee246bf52df
gc_xuanku@gagc.tv <gc_xuanku@gagc.tv>
2018-04-19 13:58:01 +0800

    no message

--- vue/vue-cli/src/App.vue
+++ vue/vue-cli/src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+	<div id="app">
+		<img src="./assets/logo.png">
+		<!--使用components文件夹下的HelloWorld组件-->
+		<HelloWorld msg="Welcome to Your Vue.js App" />
+	</div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+	import HelloWorld from './components/HelloWorld.vue'//导入组件，HelloWorld为HelloWorld.vue中export的name
+	export default {
+		name: 'app',//该组件为入口，所以main.js中render: h => h(App)
+		components: {
+			HelloWorld
+		}
+	}
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+		margin-top: 60px;
+	}
 </style>
--- vue/vue-cli/src/main.js
+++ vue/vue-cli/src/main.js
@@ -1,8 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue' //导入vue核心依赖
+import App from './App.vue' //导入组件定义为App，实例vue中render: h => h(App)
 
 Vue.config.productionTip = false
-
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+//创建vue实例
+const vm = new Vue({
+	render: h => h(App)//渲染实例的 DOM 树
+})
+vm.$mount('#app')
+// 写法演变如下
+// render: h => h(App);
+// render: h => {
+//      return h(App);
+// }
+// render: function(h) {
+//      return h(App);
+// }
+// render: function(createElement) {
+//      return createElement(App);
+// }
