b8d99fef19cafae68ffec9bf4f0bd25b4f3013aa
gc_xuanku@gagc.tv <gc_xuanku@gagc.tv>
2018-06-22 19:46:42 +0800

    <feat>新增log4js

---
+++ log4js/app.js
@@ -0,0 +1,22 @@
+const express = require('express'),
+	app = express(),
+	log = require('./utils/log4js'),
+	path = require('path'),
+	favicon = require('serve-favicon'),
+	ejs = require('ejs'),
+	session = require('express-session'),
+	cookieParser = require('cookie-parser'),
+	bodyParser = require('body-parser');
+global.log = log;
+global.app = app;
+app.use(bodyParser.json({
+	limit: '2mb'
+}));
+app.use(bodyParser.urlencoded({
+	extended: true,
+	parameterLimit: '2000'
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+const index = require('./routes/index');
+module.exports = app;
